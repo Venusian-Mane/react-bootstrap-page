90cb95211ecd96bf1d426077fe73120e5ee31519
Venusian-Mane <manelisi.mm@gmail.com>
2022-05-25 14:03:53 +0200

    Created React app and Navbar using React Bootstrap

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navigation from './components/Navbar';
+
+function App() {
+  return (
+    <div className='App'>
+      <header>
+        <Navigation />
+      </header>
+    </div>
+  );
+}
+
+export default App;
